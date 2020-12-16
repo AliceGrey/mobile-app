ae8a7334bfefabe03ded875757be84f982115e00
Matic Leva <vertical3life@gmail.com>
2020-12-16 10:56:29 +0100

    Wrap Pigeon callbacks in providers

---
+++ lib/infrastructure/pigeons/pair_provider.dart
@@ -0,0 +1,21 @@
+import 'package:cobble/infrastructure/pigeons/pigeons.dart' as pigeon;
+import 'package:hooks_riverpod/all.dart';
+
+class PairCallbacks extends StateNotifier<int> implements pigeon.PairCallbacks {
+  PairCallbacks() : super(null);
+
+  @override
+  void onWatchPairComplete(pigeon.NumberWrapper arg) {
+    state = arg.value;
+  }
+}
+
+/// Stores the address of device you are paired to. Can be null.
+final pairProvider = StateNotifierProvider<PairCallbacks>((ref) {
+  final notifier = PairCallbacks();
+  pigeon.PairCallbacks.setup(notifier);
+  ref.onDispose(() {
+    pigeon.PairCallbacks.setup(null);
+  });
+  return notifier;
+});
